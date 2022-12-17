cdb45272cf2687f21558d8671c8429b3b507f981
lcx12901 <wktl1991504424@gmail.com>
2022-12-17 23:52:44 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,46 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+mkdir customfeeds
+
+git clone --depth=1 https://github.com/DHDAXCW/packages customfeeds/packages
+git clone --depth=1 https://github.com/DHDAXCW/luci customfeeds/luci
+
+pushd customfeeds
+
+# Add luci-app-eqos
+svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-eqos luci/applications/luci-app-eqos
+
+# Add luci-proto-modemmanager
+svn co https://github.com/immortalwrt/luci/trunk/protocols/luci-proto-modemmanager luci/protocols/luci-proto-modemmanager
+
+# Add luci-app-gowebdav
+svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-gowebdav luci/applications/luci-app-gowebdav
+svn co https://github.com/immortalwrt/packages/trunk/net/gowebdav packages/net/gowebdav
+
+# Add tmate
+git clone --depth=1 https://github.com/immortalwrt/openwrt-tmate
+
+# Add gotop
+svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/admin/gotop packages/admin/gotop
+
+# Add minieap
+svn co https://github.com/immortalwrt/packages/trunk/net/minieap packages/net/minieap
+
+# Replace smartdns with the official version
+rm -rf packages/net/smartdns
+svn co https://github.com/openwrt/packages/trunk/net/smartdns packages/net/smartdns
+popd
+
+# Set to local feeds
+pushd customfeeds/packages
+export packages_feed="$(pwd)"
+popd
+pushd customfeeds/luci
+export luci_feed="$(pwd)"
+popd
+
+sed -i '/src-git packages/d' feeds.conf.default
+echo "src-link packages $packages_feed" >> feeds.conf.default
+sed -i '/src-git luci/d' feeds.conf.default
+echo "src-link luci $luci_feed" >> feeds.conf.default
